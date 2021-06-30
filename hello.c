564276d555dff3c0cd6a34b12e4872ca5288487c
Angela Jiang <to.ajiang@gmail.com>
2021-06-30 06:24:42 +0000

    Add GDB code block

--- hello.c
+++ hello.c
@@ -4,6 +4,16 @@
 int main(int argc, char ** argv)
 {
     int rank, size;
+
+		// wait to attach GDB
+		volatile int i = 0;
+    char hostname[256];
+    gethostname(hostname, sizeof(hostname));
+    printf("PID %d on %s ready for attach\n", getpid(), hostname);
+    fflush(stdout);
+    while (0 == i)
+        sleep(5);
+
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
